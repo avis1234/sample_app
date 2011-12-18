c3ef103d8433118de6aeff0289519d816abc313d
Avi Segal <avisegal@gmail.com>
2011-12-18 16:32:32 +0200

    Precompiled Stylesheets and changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,9 @@ SampleApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  
+  # Precompile  assets for Heroku
+  config.assets.precompile += %w( *.css *.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
