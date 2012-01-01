9b948f5df9f4f492e7d98319a1550061c595dfa6
Avi Segal <avisegal@gmail.com>
2012-01-01 16:44:49 +0200

    Added layout links with routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,8 @@ class PagesController < ApplicationController
 
   end
 
+  def help
+    @title = "Help"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
+  
+  root :to => 'pages#home'
+    
+  match '/contact', :to => 'pages#contact'
+  match "/help", :to => "pages#help"
+  match "/about", :to => "pages#about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
